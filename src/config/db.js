b5354c0e7b9e1b871d2941659cce57c8c3beb905
VaibhavGDeshpande <deshpande.vaibhav1012@gmail.com>
2025-10-07 13:14:53 +0530

    db changed

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017', {
       dbName: 'astrohub'
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
